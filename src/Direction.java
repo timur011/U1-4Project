afd067b50b6667433065bce07862677c8d1a212e
BT_3W2_05 <timurb2@nycstudents.net>
2022-11-29 09:26:02 -0500

    added methods

---
+++ src/Direction.java
@@ -0,0 +1,5 @@
+public class Direction {
+    public void sayDirection(){
+        System.out.println("Where would you like to go? North, East, South or West? (n, e, s, w)").
+    }
+}
--- src/ForestRunner.java
+++ src/ForestRunner.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class ForestRunner {
     public static void main(String []args){
         Scanner input = new Scanner(System.in);
+        Direction toGo = new Direction();
         System.out.println("Greetings traveler!");
         for(int j = 4; j >= 1; j--){
 
@@ -11,13 +12,35 @@ public class ForestRunner {
             }
             System.out.println();
         }
-        System.out.println("You have stumbled \n\nUpon the Great and Mysterious Magical Forest!");
+        System.out.println("You have stumbled upon \n\nThe Great and Mysterious Magical Forest!");
         System.out.println("Surely you would like to continue your journey! Type in y for yes or n for no");
         String cont = input.nextLine();
         if(cont.toUpperCase().equals("Y")){
-            System.out.println("Good choice! Now, where would you like to head? North(n), East(e), South(s), or West(w)?");
+
+
+            System.out.println("Good choice!");
+            toGo.sayDirection();
             String direction1 = input.nextLine();
-            if(direction1.toUpperCase().equals("Y")
+            if(direction1.toUpperCase().equals("N")){
+                System.out.println("The Tavern:\n\nYou see a very cozy tavern in a clearing in the forest.  The door is slightly open.  Would you like to go in? (y/n)");
+
+            }
+
+            else if(direction1.toUpperCase().equals("E")){
+                System.out.println("Good choice! Now, where would you like to head? North(n), East(e), South(s), or West(w)?");
+                }
+
+
+            else if(direction1.toUpperCase().equals("S")){
+                System.out.println("Good choice! Now, where would you like to head? North(n), East(e), South(s), or West(w)?");
+                }
+
+
+            else if(direction1.toUpperCase().equals("W")){
+                System.out.println("Good choice! Now, where would you like to head? North(n), East(e), South(s), or West(w)?");
+                }
+
+
         }else if(cont.toUpperCase().equals("N")){
             System.out.println("Uh oh! it seems like you have angered the Great Tree Ogres! Surely you would like to run(y or n)");
             String runOrNot = input.nextLine();
